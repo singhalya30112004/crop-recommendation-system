d8eb570b39ebb47c15aeba10b832824ec7a864bc
Alya Singh <pulsealya@gmail.com>
2025-07-06 15:18:30 +0530

    Add StandardScaler for feature normalisation

--- model_training.py
+++ model_training.py
@@ -18,3 +18,9 @@ X = df.drop('label', axis=1)
 y = df['label']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Feature Scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
